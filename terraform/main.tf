60f372d1683ebcbc6ae49b1b014d3025ee20d260
ashitha99 <105121638+ashitha99@users.noreply.github.com>
2024-06-20 22:55:05 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,16 +39,19 @@ resource "aws_instance" "strapi_instance" {
   vpc_security_group_ids = [aws_security_group.strapi_sg.id]  # Associate the security group
   associate_public_ip_address = true
   user_data = <<-EOF
-              #!/bin/bash
-              sudo apt-get update
-              sudo apt-get install -y nodejs npm
-              sudo npm install -g pm2
-              sudo npm install strapi@beta -g
-              strapi new my-project --dbclient=sqlite
-              cd my-project
-              echo "module.exports = { server: { host: '0.0.0.0', port: 1337 } };" > config/env/development/server.js
-              pm2 start npm --name 'strapi' -- run develop
-              sleep 360
+                                #!/bin/bash
+                                sudo apt update
+                                curl -fsSL https://deb.nodesource.com/setup_20.x -o nodesource_setup.sh
+                                sudo bash -E nodesource_setup.sh
+                                sudo apt update && sudo apt install nodejs -y
+                                sudo npm install -g yarn && sudo npm install -g pm2
+                                echo -e "skip\n" | npx create-strapi-app simple-strapi --quickstart
+                                cd simple-strapi
+                                echo "const strapi = require('@strapi/strapi');
+                                strapi().start();" > server.js
+                                pm2 start server.js --name strapi
+                                pm2 save && pm2 startup
+                                sleep 360
               EOF
 }
 
